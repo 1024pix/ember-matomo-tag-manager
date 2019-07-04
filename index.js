9a1273ce8061dd8591b9c9e49683e81ea37f9fb5
Jérémy Buget <jbuget@gmail.com>
2019-07-04 10:05:28 +0200

    Embed the Matomo tracking code.
    The integration is made thanks to #contentFor method in root file `index.js`.
    
    It requires the existence of a variable `matomo.url` in Ember app
    config/environement.js.

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 'use strict';
 
+const moduleName = require('./package').name;
+
 module.exports = {
-  name: require('./package').name
+  name: moduleName,
+
+  contentFor(type, config) {
+    if (type === 'head') {
+      if (config.matomo && config.matomo) {
+        if (config.matomo.url) {
+          const matomoUrl = config.matomo.url;
+          return `
+<!-- Matomo Tag Manager -->
+<script type="text/javascript">
+var _mtm = _mtm || [];
+_mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+g.type='text/javascript'; g.async=true; g.defer=true; g.src='${matomoUrl}'; s.parentNode.insertBefore(g,s);
+</script>
+<!-- End Matomo Tag Manager -->
+      `;
+        } else {
+          console.warn(`[${moduleName}] Expected Matomo container URL in config/environment.js`);
+        }
+      }
+    }
+  }
 };
