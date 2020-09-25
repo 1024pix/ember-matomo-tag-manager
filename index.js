8f4400c58a1daa643d431261d1c43bdb11858304
jbuget <jbuget@gmail.com>
2020-09-26 00:16:00 +0200

    Update other dependencies

--- index.js
+++ index.js
@@ -7,32 +7,32 @@ module.exports = {
 
   contentFor(type, config) {
     if (type === 'head') {
-      if (config.matomo && config.matomo) {
-        if (config.matomo.url) {
-          const matomoUrl = config.matomo.url;
-          const debugMode = config.matomo.debug ? config.matomo.debug : false;
+      if (config.matomo) {
+        if (!config.matomo.url) {
+          console.log(`[${moduleName}] No Matomo container URL has been defined in config/environment.js`);
+        }
 
-          let script = `
+        const matomoUrl = config.matomo.url;
+        const debugMode = config.matomo.debug ? config.matomo.debug : false;
+
+        let script = `
 <!-- Matomo Tag Manager -->
 <script type="text/javascript">
 var _mtm = _mtm || [];`;
 
-          if (debugMode) {
-            script += `
-_mtm.push(['enableDebugMode']);`;
-          }
-
+        if (debugMode) {
           script += `
+_mtm.push(['enableDebugMode']);`;
+        }
+
+        script += `
 _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
 var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
 g.type='text/javascript'; g.async=true; g.defer=true; g.src='${matomoUrl}'; s.parentNode.insertBefore(g,s);
 </script>
 <!-- End Matomo Tag Manager -->
 `;
-          return script;
-        } else {
-          console.warn(`[${moduleName}] Expected Matomo container URL in config/environment.js`);
-        }
+        return script;
       }
     }
   }
