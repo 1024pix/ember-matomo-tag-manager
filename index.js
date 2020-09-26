7c4790dc77810c87facd63850ceb9318ed5c8000
jbuget <jbuget@gmail.com>
2020-09-26 14:14:36 +0200

    Bugfix when MAtomo URL is not set

--- index.js
+++ index.js
@@ -10,29 +10,29 @@ module.exports = {
       if (config.matomo) {
         if (!config.matomo.url) {
           console.log(`[${moduleName}] No Matomo container URL has been defined in config/environment.js`);
-        }
+        } else {
+          const matomoUrl = config.matomo.url;
+          const debugMode = config.matomo.debug ? config.matomo.debug : false;
 
-        const matomoUrl = config.matomo.url;
-        const debugMode = config.matomo.debug ? config.matomo.debug : false;
-
-        let script = `
+          let script = `
 <!-- Matomo Tag Manager -->
 <script type="text/javascript">
 var _mtm = _mtm || [];`;
 
-        if (debugMode) {
-          script += `
+          if (debugMode) {
+            script += `
 _mtm.push(['enableDebugMode']);`;
-        }
+          }
 
-        script += `
+          script += `
 _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
 var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
 g.type='text/javascript'; g.async=true; g.defer=true; g.src='${matomoUrl}'; s.parentNode.insertBefore(g,s);
 </script>
 <!-- End Matomo Tag Manager -->
 `;
-        return script;
+          return script;
+        }
       }
     }
   }
