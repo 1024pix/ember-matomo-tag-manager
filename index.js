4a0c4c51a19aa2afb9f1ea0bbb6e73696c567c68
Vincent Hardouin <vincent.hardouin@pix.fr>
2021-03-17 16:55:49 +0100

    Remove one indentation

--- index.js
+++ index.js
@@ -6,28 +6,26 @@ module.exports = {
   name: moduleName,
 
   contentFor(type, config) {
-    if (type === 'head') {
-      if (config.matomo && config.matomo.url) {
-        const matomoUrl = config.matomo.url;
-        const debugMode = config.matomo.debug ? config.matomo.debug : false;
+    if (type === 'head' && config.matomo && config.matomo.url) {
+      const matomoUrl = config.matomo.url;
+      const debugMode = config.matomo.debug ? config.matomo.debug : false;
 
-        let startEventScript = `<!-- Matomo Tag Manager -->
-          <script type="text/javascript">
-          var _mtm = _mtm || [];`;
+      let startEventScript = `<!-- Matomo Tag Manager -->
+        <script type="text/javascript">
+        var _mtm = _mtm || [];`;
 
-        startEventScript += debugMode ? `\n _mtm.push(['enableDebugMode']);` : '';
+      startEventScript += debugMode ? `\n _mtm.push(['enableDebugMode']);` : '';
 
-        startEventScript += `
-          _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
-          </script>`;
+      startEventScript += `
+        _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+        </script>`;
 
-        let loadMatomoScript = `
-          <script type="text/javascript" async defer src="${matomoUrl}"></script>
-          <!-- End Matomo Tag Manager -->
-          `;
+      let loadMatomoScript = `
+        <script type="text/javascript" async defer src="${matomoUrl}"></script>
+        <!-- End Matomo Tag Manager -->
+        `;
 
-        return startEventScript + loadMatomoScript;
-      }
+      return startEventScript + loadMatomoScript;
     }
   }
 };
