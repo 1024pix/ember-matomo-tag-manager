61e042426d23f4dec7d748729b20f4a40ca888b9
Vincent Hardouin <vincent.hardouin@pix.fr>
2023-10-10 15:55:18 +0200

    feat: add metrics services and MatomoTagManager class

---
+++ ember-cli-matomo-tag-manager/src/services/metrics.js
@@ -0,0 +1,59 @@
+import Service from '@ember/service';
+import { getOwner } from '@ember/application';
+import MatomoTagManager from '../metrics-adapters/MatomoTagManager';
+
+export default class Metrics extends Service {
+  _adapter = null;
+
+  context = {};
+
+  enabled = typeof navigator !== 'undefined' && navigator.doNotTrack !== '1';
+
+  appEnvironment = null;
+
+  constructor() {
+    super(...arguments);
+
+    const owner = getOwner(this);
+
+    const config = owner.factoryFor('config:environment').class;
+    const { metrics = [] } = config;
+    const { environment = 'development' } = config;
+
+    this._options = { metrics, environment };
+    this.appEnvironment = environment;
+    if (!this.enabled) {
+      return;
+    }
+    this._adapter = new MatomoTagManager(this._options.metrics);
+    this._adapter.install();
+  }
+
+  identify() {
+    this.invoke('identify', ...arguments);
+  }
+
+  alias() {
+    this.invoke('alias', ...arguments);
+  }
+
+  trackEvent() {
+    this.invoke('trackEvent', ...arguments);
+  }
+
+  trackPage() {
+    this.invoke('trackPage', ...arguments);
+  }
+
+  invoke(methodName, ...options) {
+    if (!this.enabled) {
+      return;
+    }
+
+    this._adapter[methodName]({ ...this.context, ...options });
+  }
+
+  willDestroy() {
+    this._adapter.uninstall();
+  }
+}
--- test-app/config/environment.js
+++ test-app/config/environment.js
@@ -18,6 +18,11 @@ module.exports = function (environment) {
       // Here you can pass flags/options to your application instance
       // when it is created
     },
+
+    metrics: {
+      matomoUrl: 'matomo.my.example.net',
+      containerId: 'abcd123',
+    }
   };
 
   if (environment === 'development') {
@@ -26,6 +31,7 @@ module.exports = function (environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
+
   }
 
   if (environment === 'test') {
