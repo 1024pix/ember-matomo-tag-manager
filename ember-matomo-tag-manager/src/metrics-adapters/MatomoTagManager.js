626e513b0d74ccc8df44c9fd5f5842a7073b88af
Vincent Hardouin <vincent.hardouin@pix.fr>
2023-10-11 20:05:03 +0200

    feat(addon): add metrics services and MatomoTagManager adapter

---
+++ ember-matomo-tag-manager/src/metrics-adapters/MatomoTagManager.js
@@ -0,0 +1,63 @@
+import { assert } from '@ember/debug';
+
+export default class MatomoTagManager {
+  metrics = null;
+  config = null;
+
+  constructor(config) {
+    this.config = config;
+  }
+
+  toStringExtension() {
+    return 'MatomoTagManager';
+  }
+
+  install() {
+    const { matomoUrl, containerId } = this.config;
+
+    assert(
+      `[ember-matomo-tag-manager] You must pass a \`matomoUrl\` and a \`containerId\` to the ${this.toString()} adapter`,
+      matomoUrl && containerId,
+    );
+
+    this._injectScript(matomoUrl, containerId);
+  }
+
+  // prettier-ignore
+  _injectScript(matomoUrl, containerId) {
+    window._mtm = window._mtm || [];
+    window._mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.src=`https://${matomoUrl}/js/container_${containerId}.js`; s.parentNode.insertBefore(g,s);
+    g.id='matomo-tag-manager'
+  }
+
+  identify(options = {}) {
+    window._mtm.push(['setUserId', options.userId]);
+  }
+
+  trackEvent(options = {}) {
+    window._mtm.push([
+      'trackEvent',
+      options.category,
+      options.action,
+      options.name,
+      options.value,
+    ]);
+  }
+
+  trackPage(options = {}) {
+    window._mtm.push(['setCustomUrl', options.page]);
+    window._mtm.push(['trackPageView', options.title]);
+  }
+
+  uninstall() {
+    document
+      .querySelectorAll(`script[id="matomo-tag-manager"]`)
+      .forEach((el) => {
+        el.parentElement?.removeChild(el);
+      });
+
+    delete window._mtm;
+  }
+}
---
+++ test-app/tests/acceptance/index-test.js
@@ -0,0 +1,17 @@
+import { module, test } from 'qunit';
+import { visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+
+module('Acceptance | Index', function (hooks) {
+  setupApplicationTest(hooks);
+
+  test('should add Matomo script', async function (assert) {
+    await visit('/');
+    assert.strictEqual(currentURL(), '/');
+
+    const matomoScript = document.querySelector(
+      'script[id="matomo-tag-manager"]',
+    );
+    assert.ok(matomoScript);
+  });
+});
